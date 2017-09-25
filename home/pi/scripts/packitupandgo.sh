a216fee397d6d461350ec61829b3d727db055fb7
gulliverrr <github.com@koumanos.com>
2017-09-25 03:11:27 +0300

    Update packitupandgo.sh

--- home/pi/scripts/packitupandgo.sh
+++ home/pi/scripts/packitupandgo.sh
@@ -3,8 +3,5 @@
 rm -f "/etc/wpa_supplicant/wpa_supplicant.conf";
 cp /home/pi/scripts/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf;
 rm -rf "/var/log/*.*"
-history -w
-history -c
-history -w
-history -c
+rm -f "/home/pi/.bash_history"
 shutdown -h now
