9775e35370b853a9d23c535eb23f69daea83d681
gulliverrr <github.com@koumanos.com>
2017-09-29 20:30:09 +0300

    Update openhabloader.sh

--- home/pi/scripts/openhabloader.sh
+++ home/pi/scripts/openhabloader.sh
@@ -28,6 +28,7 @@ cp ./openhabloader.blank.html ./openhabloader.html
 sed -i -e "s/wlanip/$wlanip/g" ./openhabloader.html
 sed -i -e "s/wlanmac/$wlanmac/g" ./openhabloader.html
 
+pgrep chromium | xargs kill -9 #Kill all sessions in case something was hung - this will produce a restore prompt unfortunately
 sudo sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
 sudo sed -i 's/"exit_type":"Crashed"/"exit_type":"None"/' ~/.config/chromium/Default/Preferences
 sudo -u pi /usr/bin/chromium-browser --disable-restore-session-state --disable-web-security --user-data-dir --noerordialogs --disable-session-crashed-bubble --disable-infobars --kiosk /home/pi/scripts/openhabloader.html &
