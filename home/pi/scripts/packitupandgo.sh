02d44185437d06e8854747327a918d0d8bfebf42
gulliverrr <github.com@koumanos.com>
2018-02-22 01:42:38 +0200

    Empty graphs too

--- home/pi/scripts/packitupandgo.sh
+++ home/pi/scripts/packitupandgo.sh
@@ -4,4 +4,5 @@ rm -f "/etc/wpa_supplicant/wpa_supplicant.conf";
 cp /home/pi/scripts/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf;
 rm -rf "/var/log/*.*"
 rm -f "/home/pi/.bash_history"
+sudo rm -f /var/lib/openhab2/persistence/rrd4j/*.rrd
 shutdown -h now
