1f596be74175f9124fe6d828acc9265e70401aac
gulliverrr <github.com@koumanos.com>
2018-02-22 01:57:40 +0200

    Update packitupandgo.sh

--- home/pi/scripts/packitupandgo.sh
+++ home/pi/scripts/packitupandgo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm -f "/etc/wpa_supplicant/wpa_supplicant.conf";
+rm -f /etc/wpa_supplicant/wpa_supplicant.conf;
 cp /home/pi/scripts/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf;
-rm -rf "/var/log/*.*"
-rm -f "/home/pi/.bash_history"
+sudo rm -rf /var/log/*.*
+sudo rm -f /home/pi/.bash_history
 sudo rm -f /var/lib/openhab2/persistence/rrd4j/*.rrd
 shutdown -h now
