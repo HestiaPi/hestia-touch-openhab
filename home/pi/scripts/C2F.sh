4453dadd346b12ac6d7ef88d34b080016a7371d3
gulliverrr <github.com@koumanos.com>
2018-02-22 19:53:38 +0200

    Use sed
    Edit files directly rather than keeping separate files

--- home/pi/scripts/C2F.sh
+++ home/pi/scripts/C2F.sh
@@ -2,9 +2,9 @@
 
 rm -f "/home/pi/scripts/bme280.py";
 cp /home/pi/scripts/bme280F.py /home/pi/scripts/bme280.py;
-rm -f "/etc/openhab2/sitemaps/default.sitemap";
-cp /home/pi/scripts/defaultF.sitemap /etc/openhab2/sitemaps/default.sitemap;
-rm -f "/etc/openhab2/rules/default.rules";
-cp /home/pi/scripts/defaultF.rules /etc/openhab2/rules/default.rules;
-rm -f "/etc/openhab2/items/default.items";
-cp /home/pi/scripts/defaultF.items /etc/openhab2/items/default.items;
+
+sed -i 's/\/\/FAHRENHEIT/\/**FAHRENHEIT*\//g' /etc/openhab2/sitemaps/default.sitemap;
+sed -i 's/\/\*\*CELSIUS\*\//\/\/CELSIUS/g' /etc/openhab2/sitemaps/default.sitemap;
+sed -i 's/\/\/FAHRENHEIT/\/**FAHRENHEIT*\//g' /etc/openhab2/rules/default.rules;
+sed -i 's/\/\*\*CELSIUS\*\//\/\/CELSIUS/g' /etc/openhab2/rules/default.rules;
+sed -i 's/1f °C/0f °F/g' /etc/openhab2/items/default.items;
