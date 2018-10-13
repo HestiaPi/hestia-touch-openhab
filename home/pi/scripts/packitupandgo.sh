a399fddae8f55bc083222d0212002122e2f1f613
gulliverrr <github.com@koumanos.com>
2018-10-13 20:23:47 +0300

    Make it auto self expanding on next boot if SD cloned

--- home/pi/scripts/packitupandgo.sh
+++ home/pi/scripts/packitupandgo.sh
@@ -2,7 +2,8 @@
 
 rm -f /etc/wpa_supplicant/wpa_supplicant.conf;
 cp /home/pi/scripts/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf;
-sudo find /var/log -type f -delete
-sudo rm -f /home/pi/.bash_history
-sudo rm -f /var/lib/openhab2/persistence/rrd4j/*.rrd
-shutdown -h now
+sudo find /var/log -type f -delete;
+sudo rm -f /home/pi/.bash_history;
+sudo rm -f /var/lib/openhab2/persistence/rrd4j/*.rrd;
+sudo rm -f /home/pi/scripts/raspberry-pi-turnkey/pi.id;
+sudo shutdown -h now;
