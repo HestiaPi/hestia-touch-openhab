bed0a0c775c086723633e57919af135c96433a0c
gulliverrr <github.com@koumanos.com>
2019-01-17 17:48:36 +0200

    Added MyOpenHAB credentials removal

--- home/pi/scripts/packitupandgo.sh
+++ home/pi/scripts/packitupandgo.sh
@@ -6,4 +6,6 @@ sudo find /var/log -type f -delete;
 sudo rm -f /home/pi/.bash_history;
 sudo rm -f /var/lib/openhab2/persistence/rrd4j/*.rrd;
 sudo rm -f /home/pi/scripts/raspberry-pi-turnkey/pi.id;
+sudo rm -f /var/lib/openhab2/uuid;
+sudo rm -f /var/lib/openhab2/openhabcloud/secret;
 sudo shutdown -h now;
