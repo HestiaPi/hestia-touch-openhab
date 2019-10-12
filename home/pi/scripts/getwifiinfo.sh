75f391642b2e6087c8bc1b344e8ebba85888338e
gulliverrr <github.com@koumanos.com>
2019-10-12 12:55:05 +0300

    Fixes #7

--- home/pi/scripts/getwifiinfo.sh
+++ home/pi/scripts/getwifiinfo.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-/sbin/iwconfig wlan0 | grep 'Signal level' | awk '{print $4}' | cut -d= -f2 | cut -d/ -f1;
+# Returns Wifi signal strength in percentage
+
+RSSI=$(/sbin/iwconfig wlan0 | grep 'Signal level' | awk '{print $4}' | cut -d= -f2 | cut -d/ -f1);
+#RSSI=-99
+#echo $RSSI
+
+if [ -z "$RSSI" ]
+then
+  echo "-"
+elif [[ $RSSI -le -100 ]]
+then
+  echo "0"
+elif [[ $RSSI -ge -50 ]]
+then
+  echo "100"
+else
+  echo $[2 * (RSSI + 100)] | bc
+fi
 
