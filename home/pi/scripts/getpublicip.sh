f6424554773511160c16e3a08a4216eaa3681a26
gulliverrr <github.com@koumanos.com>
2019-10-12 13:15:50 +0300

    Fixes #6 partially

--- home/pi/scripts/getpublicip.sh
+++ home/pi/scripts/getpublicip.sh
@@ -5,13 +5,21 @@
 #[add this at the end, to check every hour]
 #0 * * * * /home/pi/scripts/publicip.sh
 
+ip=$(ping -c 1 ipinfo.io | awk -F'[()]' '/PING/{print $2}')
+if [ "$ip" == "192.168.4.1" ]
+ then
+ echo "Not connected" > /tmp/publicip  # overwrite for next run
+ exit 0;
+fi
+
 publicip=$(wget --timeout=10 http://ipinfo.io/ip -qO -)
 touch /tmp/publicip
- if [ "$publicip" != "$(cat /tmp/publicip)" ]
+if [ "$publicip" != "$(cat /tmp/publicip)" ]
   then
   echo $publicip > /tmp/publicip  # overwrite for next run
   # Run dyndns updater
   # dtdns is now dead :( use no-ip.com instead
   #/usr/bin/wget -O - -q -t 1 "http://www.dtdns.com/api/autodns.cfm?id=[YOUR-DTDNS-DOMAIN]&pw=[YOUR-DTDNS-PASS]&client=HestiaPiDDNSUpdater"
-  /usr/bin/wget -O - -q -t 1 "http://[USERNAME]:[PASSWORD]@dynupdate.no-ip.com/nic/update?hostname=[YOUR-HOSTNAME]"
+  #/usr/bin/wget -O - -q -t 1 "http://[USERNAME]:[PASSWORD]@dynupdate.no-ip.com/nic/update?hostname=[YOUR-HOSTNAME]"
 fi
+
