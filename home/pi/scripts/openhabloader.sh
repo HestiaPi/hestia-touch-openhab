2379921efaccc23785ba9626133a6ff402e21d40
gulliverrr <github.com@koumanos.com>
2019-10-16 02:31:01 +0300

    Update openhabloader.sh

--- home/pi/scripts/openhabloader.sh
+++ home/pi/scripts/openhabloader.sh
@@ -82,7 +82,19 @@ do
       # Load full UI
 
       echo "Easy Startup found. Loading UI...";
-      sudo cp /home/pi/scripts/default.rules /etc/openhab2/rules/default.rules;
+      if [ -f /home/pi/scripts/default.rules ]; then
+        sudo cp /home/pi/scripts/default.rules /etc/openhab2/rules/default.rules;
+      else
+        # Read /home/pi/scripts/systemtype
+        if grep -Fxq "EU" /home/pi/scripts/systemtype
+        then
+            echo "EU Loaded.";
+            sudo cp /home/pi/scripts/generic.rules /etc/openhab2/rules/default.rules;
+        else
+            echo "Default US Loaded.";
+            sudo cp /home/pi/scripts/hvac.rules /etc/openhab2/rules/default.rules;
+        fi
+      fi
       sudo chmod 755 /etc/openhab2/rules/default.rules;
       sudo chown openhab:openhab /etc/openhab2/rules/default.rules;
 
