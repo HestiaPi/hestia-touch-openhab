b1e71b52d4de97c88d89b56c1b8344e4815ed25d
gulliverrr <github.com@koumanos.com>
2019-10-19 22:16:28 +0300

    Update restore.sh

--- home/pi/scripts/restore.sh
+++ home/pi/scripts/restore.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 FILE=/home/pi/scripts/backup.zip
+OLDFILE=/home/pi/backup.zip
 if test -f "$FILE"; then
-  sudo rm /home/pi/backup.zip
-  sudo cp $FILE /home/pi/backup.zip
+  if test -f "$OLDFILE"; then
+    sudo rm $OLDFILE
+  fi
+  sudo cp $FILE $OLDFILE
   sudo reboot;
 else
     echo "No backup found."
