22d43984500d780ca9a417cbf8436a3026d6998f
gulliverrr <github.com@koumanos.com>
2019-10-20 16:34:49 +0300

    Delete status.json too
    This will cause one extra reboot in the first boot(s) but will always make sure the AP is active

--- home/pi/scripts/packitupandgo.sh
+++ home/pi/scripts/packitupandgo.sh
@@ -12,6 +12,7 @@ sudo find /var/log -type f -delete;
 sudo rm -f /home/pi/.bash_history;
 sudo rm -f /var/lib/openhab2/persistence/rrd4j/*.rrd;
 sudo rm -f /home/pi/scripts/raspberry-pi-turnkey/pi.id;
+sudo rm -f /home/pi/scripts/raspberry-pi-turnkey/status.json;
 sudo rm -f /var/lib/openhab2/uuid;
 sudo rm -f /var/lib/openhab2/openhabcloud/secret;
 sudo openhab-cli reset-ownership
