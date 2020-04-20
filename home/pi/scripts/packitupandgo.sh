09b5f4508b24b75bf6d037b45c15529e8a8a3546
gulliverrr <github.com@koumanos.com>
2020-04-20 19:34:31 +0300

    Modification for JSONDB

--- home/pi/scripts/packitupandgo.sh
+++ home/pi/scripts/packitupandgo.sh
@@ -11,11 +11,12 @@ rm -f /etc/wpa_supplicant/wpa_supplicant.conf;
 cp /home/pi/scripts/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf;
 sudo find /var/log -type f -delete;
 sudo rm -f /home/pi/.bash_history;
-sudo rm -f /var/lib/openhab2/persistence/rrd4j/*.rrd;
 sudo rm -f /home/pi/scripts/raspberry-pi-turnkey/pi.id;
 sudo rm -f /home/pi/scripts/raspberry-pi-turnkey/status.json;
 sudo rm -f /home/pi/scripts/backup.zip;
 sudo rm -f /var/lib/openhab2/uuid;
+sudo rm -f /var/lib/openhab2/persistence/rrd4j/*.rrd;
 sudo rm -f /var/lib/openhab2/openhabcloud/secret;
+sudo rm -f /var/lib/openhab2/persistence/mapdb/*;
 yes | sudo openhab-cli reset-ownership
 sudo shutdown -h now;
